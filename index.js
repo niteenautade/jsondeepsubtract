f3f67ec30212860ac23e6730f56944fba7321376
Niteen Autade <niteenautade@Niteens-MacBook-Air.local>
2018-12-22 21:26:32 +0530

    null check added

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ var jsonDeepSubtract = function (oldObj,newObj){
   var final = {};
   Object.keys(newObj).forEach((key)=>{
     if(["string","number"].includes(typeof(newObj[key]))){
-      var reg = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/
-      if(reg.test(newObj[key])){
-        if(new Date(oldObj[key]).getTime() != new Date(newObj[key]).getTime()){
-            final[key]=newObj[key]
-        }
-      }else if(oldObj[key]!=newObj[key]){
+      if(oldObj[key]!=newObj[key]){
         final[key]=newObj[key]
       }
     }else if(newObj[key] instanceof Date){
@@ -15,10 +10,10 @@ var jsonDeepSubtract = function (oldObj,newObj){
         final[key]=newObj[key]
       }
     }
-    else if(typeof(newObj[key])=="object" && !Array.isArray(newObj[key])){
+    else if(newObj[key] && typeof(newObj[key])=="object" && !Array.isArray(newObj[key])){
       final[key]=jsonDeepSubtract(oldObj[key],newObj[key])
     }
-    else if(typeof(newObj[key])=="object" && Array.isArray(newObj[key])){
+    else if(newObj[key] && typeof(newObj[key])=="object" && Array.isArray(newObj[key])){
       var arrLength = newObj[key].length
       final[key]=[]
       newObj[key].forEach( (arrEl,i) => {
@@ -32,18 +27,19 @@ var jsonDeepSubtract = function (oldObj,newObj){
           final[key].push(obj)
         }
       })
-      
-      
+    }
+    else{
+        final[key]=newObj[key]
     }
   })
   
   Object.keys(final).forEach((key,i)=>{
-    if(typeof(final[key])=="object" && !Array.isArray(final[key])){
+    if(final[key] && typeof(final[key])=="object" && !Array.isArray(final[key])){
       if(Object.keys(final[key]).length == 0){
         delete final[key]
       }
     }
-    else if(typeof(final[key])=="object" && Array.isArray(final[key])){
+    else if(final[key] && typeof(final[key])=="object" && Array.isArray(final[key])){
       if(typeof(final[key][0])=="object" && !Array.isArray(final[key][0])){
          final[key].forEach((obj,i)=>{
            if(Object.keys(obj).length == 0){
